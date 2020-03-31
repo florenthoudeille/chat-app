087bbce13d17813d7bcfa35914c0ed825c459f8e
florenthoudeille <houdeille.florent@hotmail.fr>
2020-03-31 11:59:00 +0200

    Add components et App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+        name="Pierre Simon" 
+        avatar="https://randomuser.me/api/portraits/men/22.jpg" 
+        online={true}  />
+      <Contact
+        name="Brian Roger"
+        avatar="https://randomuser.me/api/portraits/men/73.jpg"
+        online={false} />
+      <Contact
+        name="Juanito Burro"
+        avatar="https://randomuser.me/api/portraits/men/0.jpg"
+        online={true} />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,15 @@
-
-
-// className= Contact à appliquer sur balise qui ouvre et ferme le composant (enclosing tag)
-
-// faire div pour entourer la partie droite (nom + statut)
-
-// className=status sur balise qui englobera l'indicateur de statut (rouge ou vert) et le texte correstpondant
-
-// className=status-online pour le moment.
-
-// afficher ce composant quand le composant "App"
 import React from 'react';
 import './Contact.css';
 
-const online = true;
-
-function Contact() {
+function Contact(props) {
     return (
     <div className="Contact">
-        <img src="https://randomuser.me/api/portraits/men/22.jpg" alt="Homme" className="avatar" />
+        <img src={props.avatar} alt={props.name} className="avatar" />
         <div>
-            <h3 className="name">Pierre Simon</h3>
+            <h3 className="name">{props.name}</h3>
             <div className="status">
-                <img className={online ? 'status-online' : 'status-offline'} alt="" />
-                <p className='status-text'>{online ? 'online' : 'offline'}</p>
+                <img className={props.online ? 'status-online' : 'status-offline'} alt="" />
+                <p className='status-text'>{props.online ? 'online' : 'offline'}</p>
             </div>
         </div>
     </div>
